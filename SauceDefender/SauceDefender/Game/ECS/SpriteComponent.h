8355f40f57e6896a03f69c3ab9f5fab88d647963
YoiseauDesIles <johan.labrado@hotmail.fr>
2024-01-08 00:58:04 +0100

    Add sprite animation to the sprite component

--- SauceDefender/SauceDefender/Game/ECS/SpriteComponent.h
+++ SauceDefender/SauceDefender/Game/ECS/SpriteComponent.h
@@ -13,10 +13,22 @@ private:
 	SDL_Texture* texture;
 	SDL_Rect srcRect, dstRect;
 
+	bool animated = false;
+	int frames = 0;
+	//Delay between frames in ms
+	int speed = 100;
+
 public:
 	SpriteComponent() = default;
 	SpriteComponent(const std::string& path)
 	{
+		setTexture(path);
+	}
+
+	SpriteComponent(const std::string& path, int nFrames, int mSpeed)
+		:frames(nFrames), speed(mSpeed)
+	{
+		animated = true;
 		setTexture(path);
 	}
 
@@ -42,6 +54,10 @@ public:
 
 	void update() override
 	{
+		if (animated)
+		{
+			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
+		}
 
 		dstRect.x = static_cast<int>(transform->position.x);
 		dstRect.y = static_cast<int>(transform->position.y);
